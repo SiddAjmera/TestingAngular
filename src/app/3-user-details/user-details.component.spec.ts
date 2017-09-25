bdddc119d187fd93adfb9ae345f1f77e92a00767
Rajat Badjatya <rajatbadjatya@gmail.com>
2017-09-25 21:14:30 +0530

    All tests

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -2,16 +2,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Observable, Subject } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+  navigate(params) { }
+}
+
+class ActivatedRouteStub {
+
+  private subject = new Subject();
+
+  push(value) {
+    this.subject.next(value);
+  }
+
+  get params() {
+    return this.subject.asObservable();
+  }
+
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+      ]
     })
     .compileComponents();
   }));
@@ -22,7 +47,37 @@ xdescribe('UserDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+
+    it('should call navigate the user to the not-found page of the user with the specified userId is not found', () => {
+
+      let router = TestBed.get(Router);
+      let spy = spyOn(router, 'navigate');
+
+      let activatedRoute: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+      activatedRoute.push({ id: 0 });
+
+      component.ngOnInit();
+
+      expect(spy).toHaveBeenCalledWith(['not-found']);
+
+    });
+
   });
+
+  describe('save', () => {
+
+    it('should call router.navigate with relevant arguments when called', () => {
+
+      let router = TestBed.get(Router);
+      let spy = spyOn(router, 'navigate');
+
+      component.save();
+
+      expect(spy).toHaveBeenCalledWith(['users']);
+
+    });
+
+  });
+
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,67 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { AppComponent } from './app.component';
+
+class RouterModuleStub {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   }));
 
+  let fixture, app;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
+
   }));
 
   it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+
     expect(app.title).toEqual('app works!');
+
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
+
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
+
   }));
+
+  it('should have router-outlet present in it', () => {
+
+    let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+    expect(de).not.toBeNull();
+
+  });
+
+  it('should have a link to the todos page', () => {
+
+    let des = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+
+    let index = des.findIndex(de => de.attributes['routerLink'] === 'todos');
+
+    expect(index).toBeGreaterThan(-1);
+
+  });
+
 });
